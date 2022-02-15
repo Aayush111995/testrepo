495609214ac6c94bf57f29d1efc962990886e44e
Aayush Chaudhary <aayush.chaudhary@cleartrip.com>
2022-02-15 17:18:12 +0530

    insg

---
+++ src/main/java/com/test/model/NotNullIfAnotherFieldHasValue.java
@@ -0,0 +1,40 @@
+package com.test.model;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Validates that field {@code dependFieldName} is not null if
+ * field {@code fieldName} has value {@code fieldValue}.
+ **/
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Repeatable(NotNullIfAnotherFieldHasValue.List.class) // only with hibernate-validator >= 6.x
+@Constraint(validatedBy = NotNullAnotherFieldHasValueValidator.class)
+@Documented
+public @interface NotNullIfAnotherFieldHasValue {
+
+    String fieldName();
+    String[] dependFieldName();
+    String fieldValue() default "null";
+   String dependFieldObject() default "null";
+    String message() default "{NotNullIfAnotherFieldHasValue.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({TYPE, ANNOTATION_TYPE})
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        NotNullIfAnotherFieldHasValue[] value();
+    }
+
+}
